b6f4069d148638673d87b521d7dbffb5b6de5c06
Hardjowirogo <Tobias.Hardjowirogo@haw-hamburg.de>
2019-12-09 14:15:32 +0100

    initial

---
+++ SensorListe.h
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file    	SensorListe.h 
+  * @author  	Jonathan Backes (Matr.-Nr. 2394482), Tobias Hardjowirogo (Matr.-Nr. 2387188)
+  * @task			WS19_A4 1-Wire Kommunikation       	  
+  * @version 	V1.1
+  * @date    	09.12.2019
+  * @brief   	
+  ******************************************************************************
+  */
+	
+	
+	
+	#ifndef	SensorListe
+	#define SensorListe
+	
+	
+	
+	
+	#endif
+	
+	
+	
+	
---
+++ main.c
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file    	main.c 
+  * @author  	Jonathan Backes (Matr.-Nr. 2394482), Tobias Hardjowirogo (Matr.-Nr. 2387188)
+  * @task			WS19_A4 1-Wire Kommunikation       	  
+  * @version 	V1.1
+  * @date    	09.12.2019
+  * @brief   	Main program body
+  ******************************************************************************
+  */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include "TI_Lib.h"
+#include "tft.h"
+#include "timer.h"
+
+//--- For GPIOs -----------------------------
+//Include instead of "stm32f4xx.h" for
+//compatibility between Simulation and Board
+//#include "TI_memory_map.h"
+
+
+
+
+/**
+  * @brief  Main program
+  * @param  None
+  */
+int main(void)
+{
+	
+
+
+  return 0;
+
+}
+// EOF
